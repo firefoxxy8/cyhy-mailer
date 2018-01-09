89859d648a026499b8c7335cb10964d1ce7b6c2f
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-01-09 07:58:52 -0500

    Some modified language from Cameron to accompany the trustymail reports

--- cyhy/mailer/TmailMessage.py
+++ cyhy/mailer/TmailMessage.py
@@ -27,16 +27,19 @@ class TmailMessage(ReportMessage):
 
     TextBody = '''Greetings {{acronym}},
 
-Attached is your Trustworthy Email Report. This report presents your organization's support of SPF and DMARC, two email authentication standards, as published at your .gov domains. The data in this report comes from a scan that took place on {{report_date}}.
+Attached is your Trustworthy Email Report. This report presents your organization's use of SPF, DMARC, and STARTTLS, as discovered at your .gov domains. The data in this report comes from a scan that took place on {{report_date}}.
 
 DHS Binding Operational Directive 18-01 requires your agency to take certain actions relevant to the data in this report:
-* Within 90 days of BOD issuance, configure all second-level domains to have valid SPF/DMARC records, with at minimum a DMARC policy of "p=none" and at least one address defined as a recipient of aggregate and/or failure reports.
-* Within one year of BOD issuance, set a DMARC policy of "reject" for all second-level domains and mail-sending hosts.
-* The Directive additionally requires all internet-facing mail servers to offer STARTTLS. This data will be represented in the Trustworthy Email Report in December.
+* By January 15, 2018:
+  * Configure all second-level domains to have valid SPF/DMARC records, with at minimum a DMARC policy of "p=none".
+  * "reports@dmarc.cyber.dhs.gov" must be added as a recipient of aggregate reports.
+  * Internet-facing mail servers must offer STARTTLS.
+* By October 16, 2018:
+  * A DMARC policy of reject must be set for all second-level domains and mail-sending hosts.
 
 Raw results in this Report are available as a CSV in the Appendix, which includes error messages. Note that this report includes data from second-level .gov domains, but the Directive's scope includes any domain suffix.
 
-The actions the Directive requires increase the security of emails in transit and makes it easier to detect emails that attempt to spoof .gov domains. This protects intra-government users and the general public equally. See https://cyber.dhs.gov/#intro for more information about email authentication, and https://cyber.dhs.gov/#guide for a compliance checklist and FAQ.
+See https://cyber.dhs.gov/intro for more information about email authentication, and https://cyber.dhs.gov/guide for a compliance checklist and FAQ.
 
 If you believe our reporting or methodology is in error, let us know. If the flaw appears tool-related, we encourage you to file an issue on our open-source scanner at https://github.com/dhs-ncats/trustymail.
 
@@ -51,26 +54,34 @@ U.S. Department of Homeland Security
 ncats@hq.dhs.gov
 
 ----changelog----
+1/09/2018
+* Added: STARTTLS tracking, BOD scoring of available data, SPF tracking solely for second-level domains and SMTP-responsive subdomains
+
 12/11/2017
 * Known issue: The fed.us TLD is inaccurately represented in some reports.
------------------
+--------------------
 '''
 
     HtmlBody = '''<html>
 <head></head>
-<body>
+<body>  
 <div style=""font-size:14.5"">
 <p>Greetings {{acronym}},</p>
-<p>Attached is your <b>Trustworthy Email Report</b>. This report presents your organization's support of SPF and DMARC, two email authentication standards, as published at your .gov domains. The data in this report comes from a <b>scan that took place on {{report_date}}.</b></p>
+<p>Attached is your <b>Trustworthy Email Report</b>. This report presents your organization's use of SPF, DMARC, and STARTTLS, as discovered at your .gov domains. The data in this report comes from a <b>scan that took place on {{report_date}}.</b></p>
 <p><a href="https://cyber.dhs.gov">DHS Binding Operational Directive 18-01</a> requires your agency to take certain actions relevant to the data in this report:</p>
+<p><i>By January 15, 2018</i>:
 <ul>
-<li>Within 90 days of BOD issuance, configure all second-level domains to have valid SPF/DMARC records, with at minimum a DMARC policy of &ldquo;p=none&rdquo; and at least one address defined as a recipient of aggregate and/or failure reports.</li>
-<li>Within one year of BOD issuance, set a DMARC policy of &ldquo;reject&rdquo; for all second-level domains and mail-sending hosts.</li>
-<li>(The Directive additionally requires all internet-facing mail servers to offer STARTTLS. This data will be represented in the Trustworthy Email Report in December.</li>
+<li>Configure all second-level domains to have valid SPF/DMARC records, with at minimum a DMARC policy of &ldquo;p=none&rdquo;.</li>
+<li>&ldquo;reports@dmarc.cyber.dhs.gov&rdquo; <a href="https://cyber.dhs.gov/guide/#dhs-dmarc-reporting-location">must be added as a recipient of aggregate reports</a>.</li>
+<li>Internet-facing mail servers must offer STARTTLS.</li>
+</ul>
+<i>By October 16, 2018</i>:
+<ul>
+<li>A DMARC policy of reject must be set for all second-level domains and mail-sending hosts.</li>
 </ul>
 </p>
 <p>Raw results in this Report are available as a CSV in the Appendix, which includes error messages. Note that this report includes data from second-level .gov domains, but the Directive's <a href="https://cyber.dhs.gov/guide/#what-is-the-scope-of-bod-18-01">scope includes any domain suffix</a>.</p>
-<p>The actions the Directive requires increases the security of emails in transit and makes it easier to detect emails that attempt to spoof .gov domains. This protects intra-government users and the general public equally. See <a href="https://cyber.dhs.gov/#intro">cyber.dhs.gov/#intro</a> for more information about email authentication, and <a href="https://cyber.dhs.gov/#guide">cyber.dhs.gov/#guide</a> for a compliance checklist and FAQ.</p>
+<p>See <a href="https://cyber.dhs.gov/intro">cyber.dhs.gov/intro</a> for more information about email authentication, and <a href="https://cyber.dhs.gov/guide">cyber.dhs.gov/guide</a> for a compliance checklist and FAQ.</p>
 <p>If you believe our reporting or methodology is in error, let us know. If the flaw appears tool-related, we encourage you to <a href="https://github.com/dhs-ncats/trustymail">file an issue</a> on our open-source scanner.</p>
 <p>We welcome your feedback and questions.</p>
 Cheers,<br>
@@ -81,6 +92,8 @@ U.S. Department of Homeland Security <br/>
 <a href=""mailto:ncats@hq.dhs.gov""> ncats@hq.dhs.gov </a> </p>
 </div></p><br>
 <p>----changelog----</p>
+<p><i>1/09/2018</i></p>
+<p><b>* Added:</b> STARTTLS tracking, BOD scoring of available data, SPF tracking solely for second-level domains and SMTP-responsive subdomains<br></p>
 <p><i>12/11/2017</i></p>
 <b>* Known issue:</b> The fed.us TLD is inaccurately represented in some reports.</p>
 <p>--------------------</p>
